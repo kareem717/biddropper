cc8f96115eac93d0620c7b7e91f0994d9311dbd5
kareem717 <kareem.717@icloud.com>
2024-06-24 15:14:55 -0400

    improve industry select

--- src/components/app/IndustrySelect.tsx
+++ src/components/app/IndustrySelect.tsx
@@ -26,7 +26,7 @@ export interface IndustrySelectProps extends ComponentPropsWithoutRef<typeof Com
 }
 
 export const IndustrySelect: FC<IndustrySelectProps> = ({ className, onIndustriesSelect, ...props }) => {
-  const { selectedIndustries, setSelectedIndustries } = useIndustrySelect();
+  const { getSelectedIndustries, setSelectedIndustries } = useIndustrySelect();
 
   const { data: industries, isLoading, isError } = trpc.industry.getIndustries.useQuery();
 
@@ -36,11 +36,12 @@ export const IndustrySelect: FC<IndustrySelectProps> = ({ className, onIndustrie
     });
   }
 
-  const handleSelect = (industry: Industry) => {
-    setSelectedIndustries(selectedIndustries.includes(industry) ? selectedIndustries.filter((i) => i.id !== industry.id) : [...selectedIndustries, industry])
-    onIndustriesSelect && onIndustriesSelect(selectedIndustries)
-  }
+  const selectedIndustries = getSelectedIndustries();
 
+  const handleSelect = (industry: Industry) => {
+    setSelectedIndustries(getSelectedIndustries().includes(industry) ? getSelectedIndustries().filter((i) => i.id !== industry.id) : [...getSelectedIndustries(), industry])
+    onIndustriesSelect && onIndustriesSelect(getSelectedIndustries())
+  }
   return (
     <Popover>
       <PopoverTrigger asChild>
@@ -49,7 +50,7 @@ export const IndustrySelect: FC<IndustrySelectProps> = ({ className, onIndustrie
           role="combobox"
           className="w-full justify-between"
         >
-          {selectedIndustries && selectedIndustries.length > 0
+          {selectedIndustries.length > 0
             ? `${selectedIndustries.length} selected`
             : "Select industry..."}
           <Icons.chevronUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
@@ -66,21 +67,29 @@ export const IndustrySelect: FC<IndustrySelectProps> = ({ className, onIndustrie
             <CommandGroup>
               {isLoading ?
                 <CommandItem>Loading...</CommandItem>
-                : industries && industries.map((industry) => (
-                  <CommandItem
-                    key={industry.id}
-                    value={industry.id}
-                    onSelect={() => handleSelect(industry)}
-                  >
-                    <Icons.check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        selectedIndustries.includes(industry) ? "opacity-100" : "opacity-0"
+                : industries && industries
+                  .sort((a, b) => {
+                    // Rank selected industries at top
+                    const aSelected = selectedIndustries.some((selected) => selected.id === a.id) ? 1 : 0;
+                    const bSelected = selectedIndustries.some((selected) => selected.id === b.id) ? 1 : 0;
+                    return bSelected - aSelected;
+                  })
+                  .map((industry) => (
+                    <CommandItem
+                      key={industry.id}
+                      value={industry.name}
+                      onSelect={() => handleSelect(industry)}
+                    >
+                      {selectedIndustries.some((selected) => selected.id === industry.id) && (
+                        <Icons.check
+                          className={cn(
+                            "mr-2 h-4 w-4",
+                          )}
+                        />
                       )}
-                    />
-                    {industry.name}
-                  </CommandItem>
-                ))}
+                      {industry.name}
+                    </CommandItem>
+                  ))}
             </CommandGroup>
           </CommandList>
         </Command>
