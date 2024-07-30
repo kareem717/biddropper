88a356ad9c4b478e9cda4b34363fdd3975026e80
kareem717 <kareem.717@icloud.com>
2024-07-30 12:37:34 -0400

    err loading 404 pages

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,11 @@
 'use client' // Error components must be Client Components
 
+import { LogoDiv } from '@/components/app/LogoDiv'
+import { Icons } from '@/components/Icons'
+import { Button, buttonVariants } from '@/components/ui/button'
+import redirects from '@/config/redirects'
+import { cn } from '@/lib/utils'
+import Link from 'next/link'
 import { useEffect } from 'react'
 
 export default function Error({
@@ -15,16 +21,22 @@ export default function Error({
   }, [error])
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="flex flex-col items-center gap-4 justify-center h-screen w-full">
+      <Icons.biddropper className='w-40 h-40' />
+      <h2 className='text-2xl font-semibold'>Something went wrong!</h2>
+      <div className='flex gap-4 w-1/3 max-w-md min-w-[200px]'>
+        <Button
+          onClick={
+            () => reset()
+          }
+          className='w-full'
+        >
+          Try again
+        </Button>
+        <Link href={redirects.home} className={cn(buttonVariants({ variant: 'secondary' }), 'w-full')}>
+          Go back to home
+        </Link>
+      </div>
     </div>
   )
 }
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,11 +1,24 @@
+import redirects from "@/config/redirects";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function NotFound() {
 	return (
-		<div>
-			<h2>Not Found</h2>
-			<p>Could not find requested resource</p>
-			<Link href="/">Return Home</Link>
+		<div className="flex flex-col items-center justify-center h-screen w-full gap-4">
+			<div className="flex items-center justify-center gap-2 text-8xl font-bold">
+				<span className="text-primary">4</span>
+				<span>0</span>
+				<span className="text-primary">4</span>
+				<span>!</span>
+			</div>
+			<p className="text-center text-xl text-muted-foreground">We couldn't find what you were looking for</p>
+			<Link
+				href={redirects.home}
+				className={cn(buttonVariants(), 'px-8')}
+			>
+				Return Home
+			</Link>
 		</div>
 	);
 }
